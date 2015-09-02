25f6a2bcf1fc4715537c3e99e88027dbfe89d129
Hugo Tunius <hugo.tunius@gmail.com>
2015-09-02 11:06:38 +0200

    Initial Commit

---
+++ SwiftObjLoader/SwiftObjLoader.h
@@ -0,0 +1,19 @@
+//
+//  SwiftObjLoader.h
+//  SwiftObjLoader
+//
+//  Created by Hugo Tunius on 02/09/15.
+//  Copyright © 2015 Hugo Tunius. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftObjLoader.
+FOUNDATION_EXPORT double SwiftObjLoaderVersionNumber;
+
+//! Project version string for SwiftObjLoader.
+FOUNDATION_EXPORT const unsigned char SwiftObjLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftObjLoader/PublicHeader.h>
+
+
